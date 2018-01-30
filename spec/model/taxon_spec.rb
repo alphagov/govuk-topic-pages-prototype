5ce8601be762fa726b50d306af3ad67c3aeaa091
Felix Harrison <fmlharrison@gmail.com>
2018-01-30 12:04:10 +0000

    Add content_id method to Taxon class
    Add test helper to get example content items

--- spec/model/taxon_spec.rb
+++ spec/model/taxon_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 require 'gds_api/test_helpers/content_store'
 
 RSpec.describe Taxon, type: :model do
-  include GdsApi::TestHelpers::ContentStore
+  include ContentSchemaHelpers
 
   describe "#content_item" do
     before(:each) do
-      example_schema_string = GovukContentSchemaTestHelpers::Examples.new.get('taxon', 'taxon_with_child_taxons')
-      @example_schema = JSON.parse(example_schema_string)
+      @example_schema = example_content_schema("taxon", "taxon_with_child_taxons")
       @topic_taxon = Taxon.new("education")
     end
 
@@ -33,17 +32,26 @@ RSpec.describe Taxon, type: :model do
     end
 
     it "should indicate that taxon has child taxons" do
-      example_schema_with_children = JSON.parse(GovukContentSchemaTestHelpers::Examples.new.get('taxon', 'taxon_with_child_taxons'))
+      example_schema_with_children = example_content_schema('taxon', 'taxon_with_child_taxons')
       allow(Services.content_store).to receive(:content_item).with("/education").and_return(example_schema_with_children)
 
       expect(@topic_taxon.children?).to be_truthy
     end
 
     it "should indicate that taxon does not have child taxons" do
-      example_schema_without_children = JSON.parse(GovukContentSchemaTestHelpers::Examples.new.get('taxon', 'taxon'))
+      example_schema_without_children = example_content_schema('taxon', 'taxon')
       allow(Services.content_store).to receive(:content_item).with("/education").and_return(example_schema_without_children)
 
       expect(@topic_taxon.children?).to be_falsey
+    end
+  end
+
+  describe "#content_id" do
+    it "should return the correct content id" do
+      content_item = example_content_schema('taxon', 'taxon')
+      allow(Services.content_store).to receive(:content_item).with("/education").and_return(content_item)
+
+      expect(Taxon.new("education").content_id).to eq("63d98739-fc60-4e28-a65f-edb56000fd39")
     end
   end
 end

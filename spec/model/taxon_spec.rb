297d5e5a9e76ca6f5b3f09e16eefb720d47b8c1b
Felix Harrison <fmlharrison@gmail.com>
2018-02-02 16:44:24 +0000

    Add tagged_content_count method to Taxon class
    This method is used to return the count of tagged
    content for a particular content_id.

--- spec/model/taxon_spec.rb
+++ spec/model/taxon_spec.rb
@@ -3,6 +3,7 @@ require 'gds_api/test_helpers/content_store'
 
 RSpec.describe Taxon, type: :model do
   include ContentSchemaHelpers
+  include ServicesRequestHelpers
 
   describe "#content_item" do
     before(:each) do
@@ -11,13 +12,13 @@ RSpec.describe Taxon, type: :model do
     end
 
     it "should return content item title" do
-      allow(Services.content_store).to receive(:content_item).with("/education").and_return(@example_schema)
+      stub_content_store("/education", @example_schema)
 
       expect(@topic_taxon.title).to eq("Taxon with children")
     end
 
     it "should return associated child taxons" do
-      allow(Services.content_store).to receive(:content_item).with("/education").and_return(@example_schema)
+      stub_content_store("/education", @example_schema)
 
       child_taxons = @topic_taxon.child_taxons
 
@@ -33,14 +34,14 @@ RSpec.describe Taxon, type: :model do
 
     it "should indicate that taxon has child taxons" do
       example_schema_with_children = example_content_schema('taxon', 'taxon_with_child_taxons')
-      allow(Services.content_store).to receive(:content_item).with("/education").and_return(example_schema_with_children)
+      stub_content_store("/education", example_schema_with_children)
 
       expect(@topic_taxon.children?).to be_truthy
     end
 
     it "should indicate that taxon does not have child taxons" do
       example_schema_without_children = example_content_schema('taxon', 'taxon')
-      allow(Services.content_store).to receive(:content_item).with("/education").and_return(example_schema_without_children)
+      stub_content_store("/education", example_schema_without_children)
 
       expect(@topic_taxon.children?).to be_falsey
     end
@@ -49,9 +50,19 @@ RSpec.describe Taxon, type: :model do
   describe "#content_id" do
     it "should return the correct content id" do
       content_item = example_content_schema('taxon', 'taxon')
-      allow(Services.content_store).to receive(:content_item).with("/education").and_return(content_item)
+      stub_content_store("/education", content_item)
 
       expect(Taxon.new("education").content_id).to eq("63d98739-fc60-4e28-a65f-edb56000fd39")
+    end
+  end
+
+  describe "#child_taxon_tagged_content_count" do
+    it "should return the count for a child taxon tagged content" do
+      example_schema_with_children = example_content_schema('taxon', 'taxon_with_child_taxons')
+      stub_content_store("/education", example_schema_with_children)
+      stub_rummager(example_schema_with_children["content_id"])
+
+      expect(Taxon.new("education").tagged_content_count(example_schema_with_children["content_id"])).to eq(2)
     end
   end
 end
